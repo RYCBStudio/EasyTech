5229ca42cb26b2d68a93b58df489049e564e9ed5
QYF-RYCBStudio <qyf090506@163.com>
2022-06-18 15:00:18 +0800

    Update EasyTech.java

--- src/main/java/com/rycb/etech/EasyTech.java
+++ src/main/java/com/rycb/etech/EasyTech.java
@@ -694,7 +694,7 @@ public class EasyTech
 {
     public static final String MODID = "etech";
     public static final String NAME = "EasyTech";
-    public static final String MCVERSION = "1.12.2";
+    public static final String MCVERSION = "1.18.1";
     public static final String VERSION = "1.0";
 
     @Mod.Instance(EasyTech.MODID)
